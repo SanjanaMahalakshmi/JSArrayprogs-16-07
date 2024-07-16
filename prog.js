c25402b0045679c01afbce25494f15f8aa91f658
SanjanaMahalakshmi <86121411+SanjanaMahalakshmi@users.noreply.github.com>
2024-07-16 17:13:28 +0530

    Add files via upload

---
+++ prog.js
@@ -0,0 +1,84 @@
+           //Program1-Array Initalization and Declaration
+
+           let colours=["Blue","Pink","Yellow","Purple"]
+           document.write(colours)
+           
+         //Program 2-Array Manipulation
+
+
+// function addele(arr) 
+// {   
+//     arr.push("Lavender");
+//     return "Adding an element we get: "+arr
+// }
+// document.write(addele(["Purple","Blue","Pink","Black"]))
+
+
+
+// let arr1=["Purple","Blue","Pink","Black"]
+// arr1.pop()
+// document.write("<br>","Popping an element: ",arr1)
+
+// let arr2=["Purple","Blue","Pink","Black"]
+// arr2.shift()
+// document.write("<br>","Shifitng an element we get: ",arr2)
+
+// let arr3=["Purple","Blue","Pink","Black"]
+// arr3.unshift("Purple")
+// document.write("<br>","Unshifting an element we get: ",arr3)
+
+
+
+
+           //Program 3-Sorting and reversing
+// let numbers=[94,95,92,93,91,90,98,96,97,99]
+// let num1=numbers.sort();
+// let num2=num1.reverse();
+// document.write(num2)
+   
+
+              //Program 4- Maximum and Minimum
+// let max=0;
+
+// function array(numbers)
+// {
+// for(let i=0;i<numbers.length;i++)
+// {
+//     if(numbers[i]>max)
+//     {
+//         max=numbers[i];
+//     }
+    
+
+// }
+// return max;
+// }
+// function array1(numbers)
+// {
+//   return Math.min(...numbers);
+// }
+// document.write("Maximum among the arrays is: "+ array([100,200,500,10000,9999,565,765]  ))
+// document.write("<br>"+"Minimum among the arrays is: "+ array1([100,200,500,10000,9999,565,765] ))
+
+
+               //Program 5-Flitering
+// let arr=[5,6,7,8,10]
+// let res=arr.filter(num);
+// function num(arr){
+//     return arr>7
+// }
+// document.write("Filtering Elements we get: ",res)
+
+
+            //    Program 6-Joining and Splitting
+
+// let array1=["She like to dance","sing"]
+// let res=array1.join(" and ")
+// document.write(res,"<br>")
+
+// let arr=["hey","how","are","you","Nice","to","meet","you"];
+// let firstpart=arr.slice(0,4);
+// let lastpart=arr.slice(4,8);
+
+// document.write("Splitting an array we get: ",firstpart,"<br>");
+// document.write(lastpart)
